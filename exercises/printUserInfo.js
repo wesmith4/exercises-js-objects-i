a7e3ab5fe8f2f19db86db4e2d52a4b4b064a30ad
Will Smith <wismith@davidson.edu>
2020-02-03 13:44:38 -0800

    printUserInfo update

--- exercises/printUserInfo.js
+++ exercises/printUserInfo.js
@@ -7,11 +7,30 @@ let process = require('process');
 
 let user = {};
 
-user['firstName'] = '<Change This>';
-user['lastName'] = '<Change This>';
-user['currentLocation'] = '<Change This>';
-user['favoriteSongs'] = ['One Song', 'Another Great Song', 'Last Great Song'];
-user['age'] = 108;
+user['firstName'] = 'Han';
+user['lastName'] = 'Solo';
+user['currentLocation'] = 'Tatooine';
+user['favoriteSongs'] = ['Talledega', 'FloraBama', 'Outskirts of Heaven'];
+user['age'] = 70;
+user['favoriteFood'] = 'desert rat';
+user['favoriteMovies'] = ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi'];
+user['printFavorites'] = function() {
+  console.log('Your favorite songs: ');
+  for (let song of this.favoriteSongs) {
+    console.log(`${this.favoriteSongs.indexOf(song) + 1}: ${song}`);
+  }
+  console.log();
+  console.log('Your favorite movies: ');
+  for (let movie of this.favoriteMovies) {
+    console.log(`${this.favoriteMovies.indexOf(movie) + 1}: ${movie}`);
+  }
+  console.log();
+  if (this.favoriteFood === 'apples') {
+    console.log(`Your favorite food is ${this.favoriteFood}! You've kept the doctor away!`);
+  } else {
+    console.log(`Your favorite food is ${this.favoriteFood}`);
+  }
+}
 
 // To get started, set user['canBeDisplayed'] to true instead of false
 user['canBeDisplayed'] = true;
@@ -30,13 +49,8 @@ console.log();
 if (user['age'] >= 100) {
   console.log(`Wow! You're ${user['age']} years old? Fewer than 0.2% reach that age.`);
   console.log();
+} else {
+  console.log(`I see you're ${user['age']} years old.  When's your birthday?`);
 }
 
-console.log('Looks like you have great taste in music. Your favorite songs:');
-
-for (let i = 0; i < user['favoriteSongs'].length; i += 1) {
-  let songPosition = i + 1;
-  let songName = user['favoriteSongs'][i];
-
-  console.log(`${songPosition}. ${songName}`);
-}
+user.printFavorites();
